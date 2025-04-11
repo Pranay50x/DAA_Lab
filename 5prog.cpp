378e9787efd45a758de36b2c2b97d88cdecf3329
Pranay K R <2005pranaykr@gmail.com>
2025-04-11 19:53:50 +0530

    added adj matrix

--- 5prog.cpp
+++ 5prog.cpp
@@ -6,72 +6,65 @@
 
 using namespace std;
 
-class Solution{
-	public:
-		// unchanged
-		vector<int> bfsGraph(int V, vector<int> adj[]){
-			vector<int> vis(V+1, 0); ;
-			vis[1] = 1;
-			queue<int> q;
+class Solution {
+public:
+    vector<int> bfsGraph(int V, vector<vector<int>>& adjMat) {
+        vector<int> vis(V + 1, 0);
+        vis[1] = 1;
+        queue<int> q;
 
-			vector<int> bfs;
+        vector<int> bfs;
+        q.push(1);
 
-			q.push(1);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
 
-			while(!q.empty()){
-				int node = q.front();
-				q.pop();
-				bfs.push_back(node);
+            for (int i = 1; i <= V; i++) {
+                if (adjMat[node][i] == 1 && !vis[i]) {
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
 
-				for(auto x : adj[node]){
-					if(!vis[x]){
-						vis[x] = 1;
-						q.push(x);
-					}
-				}
-			}
-			return bfs;
-		}
+        return bfs;
+    }
 };
 
-void addEdge(vector<int> adj[], int u, int v){
-	adj[u].push_back(v);
-	adj[v].push_back(u);
+void printA(const vector<int>& ans) {
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
 }
 
-void printA(const vector<int> &ans){
-	for(int x : ans){
-		cout << x << " ";
-	}
-	cout << "\n";
-}
+int main() {
+    int V, E;
+    cout << "Enter number of vertices and edges: ";
+    cin >> V >> E;
 
-int main(){
-	int V, E;
-	cout << "Enter number of vertices and edges: ";
-	cin >> V >> E;
+    vector<vector<int>> adjMat(V + 1, vector<int>(V + 1, 0));
 
-	// make room for 1..V
-	vector<int>* adj = new vector<int>[V+1];
+    cout << "Enter each edge (u v) on its own line:\n";
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adjMat[u][v] = 1;
+        adjMat[v][u] = 1; // because the graph is undirected
+    }
 
-	cout << "Enter each edge (u v) on its own line:\n";
-	for(int i = 0; i < E; i++){
-		int u, v;
-		cin >> u >> v;
-		addEdge(adj, u, v);
-	}
+    clock_t st = clock();
 
-	clock_t st = clock();
+    Solution sol;
+    vector<int> fin = sol.bfsGraph(V, adjMat);
 
-	Solution sol;
-	vector<int> fin = sol.bfsGraph(V, adj);
+    printA(fin);
 
-	printA(fin);
+    clock_t et = clock();
+    double time = double(et - st) / CLOCKS_PER_SEC;
+    cout << "Time used is: " << fixed << setprecision(6) << time << " s\n";
 
-	clock_t et = clock();
-	double time = double(et - st) / CLOCKS_PER_SEC;
-	cout << "Time used is: " << fixed << setprecision(6) << time << " s\n";
-
-	delete[] adj;
-	return 0;
+    return 0;
 }
