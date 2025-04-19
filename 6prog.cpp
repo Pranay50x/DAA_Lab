fa6716ac73ed088dec7d0c18f65278153cd8ef43
Pranay K R <2005pranaykr@gmail.com>
2025-04-19 07:29:16 +0530

    ,

--- 6prog.cpp
+++ 6prog.cpp
@@ -2,64 +2,63 @@
 #include <iomanip>
 #include <vector>
 #include <stack>
-#include <algorithm>
 #include <ctime>
 
 using namespace std;
 
-void addEdge(vector<int> adj[], int u, int v){
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+void addEdge(vector<vector<bool>> &matrix, int u, int v) {
+    matrix[u][v] = true;
+    matrix[v][u] = true;
 }
 
-
-void dfsStack(int V, vector<int> adj[], int start){
-    vector<bool> visited(V+1, false); // this is array of vectors the adj[] one
-    stack <int> st;
-    // pushing the first node into the stack
+void dfsStack(int V, vector<vector<bool>> &matrix, int start) {
+    vector<bool> visited(V+1, false);
+    stack<int> st;
     st.push(start);
 
-
-    while(!st.empty()){
+    while(!st.empty()) {
         int node = st.top();
-
         st.pop();
-        //returning the output and marking the node as visited in the array
-        if(!visited[node]){
-            cout << node << " ";
-            visited[node] =true;
-        }
-        // traversing the adjaceny matrix and then pushing the neighbour of the node we visited already.
-        for(int i = adj[node].size()-1; i>=0;i--){
-            int neighbour = adj[node][i];
 
-            if(!visited[neighbour]){
-                st.push(neighbour);
+        if(!visited[node]) {
+            cout << node << " ";
+            visited[node] = true;
+        }
+
+        // Check neighbors in reverse order to maintain DFS sequence
+        for(int i = V; i >= 1; i--) {
+            if(matrix[node][i] && !visited[i]) {
+                st.push(i);
             }
         }
     }
 }
 
-int main(){
-    clock_t st,et;
+int main() {
+    clock_t st, et;
     st = clock();
-    cout << "Enter the number of vertices and edges: " ;
-    int V,E;
+
+    cout << "Enter the number of vertices and edges: ";
+    int V, E;
     cin >> V >> E;
 
-    vector<int> adj[V+1]; //declaring and using array of vectors
+    // Initialize adjacency matrix
+    vector<vector<bool>> matrix(V+1, vector<bool>(V+1, false));
 
     cout << "Enter pair of edges and vertices: \n";
-    for(int i = 0;i<E;i++){
-        int u,v;
+    for(int i = 0; i < E; i++) {
+        int u, v;
         cin >> u >> v;
-        addEdge(adj, u ,v);
+        addEdge(matrix, u, v);
     }
+
     cout << "DFS Traversal: \n";
-    dfsStack(V, adj, 1);
+    dfsStack(V, matrix, 1);
     cout << endl;
+
     et = clock();
-    double time_used = (double(et-st))/CLOCKS_PER_SEC;
+    double time_used = (double)(et - st) / CLOCKS_PER_SEC;
     cout << "Time used is: " << fixed << setprecision(6) << time_used << " s\n";
+
     return 0;
 }
